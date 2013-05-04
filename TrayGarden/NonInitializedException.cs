34e6dc8ed60f9f97b360be9a347364affa484a66
Alex <user.home.0000@gmail.com>
2013-05-04 03:05:13 +0300

    Intoducing NonInitializedException.

---
+++ TrayGarden/NonInitializedException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TrayGarden
+{
+    public class NonInitializedException:Exception
+    {
+        public NonInitializedException():base("Object isn't properly initialized. Should be initialized before usage.")
+        {
+            
+        }
+    }
+}
