53c816c09528c8299b43c1e37181a3ed2a6a6ba8
Alex Povar <user.home.0000@gmail.com>
2013-05-17 22:44:31 +0300

    Introduce PlantServiceBase

--- TrayGarden/Services/PlantServiceBase.cs
+++ TrayGarden/Services/PlantServiceBase.cs
@@ -2,10 +2,49 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using TrayGarden.Plants;
 
 namespace TrayGarden.Services
 {
-    class PlantServiceBase
+    public abstract class PlantServiceBase<TPlantLuggageType>:IService where TPlantLuggageType : class
     {
+
+        public string LuggageName { get; set; }
+
+
+        protected TPlantLuggageType GetPlantLuggage(IPlant plant)
+        {
+            if (!plant.HasLuggage(LuggageName))
+                return null;
+            return plant.GetLuggage<TPlantLuggageType>(LuggageName);
+        }
+
+        protected virtual void PlantOnEnabledChanged(IPlant plant, bool newValue)
+        {
+
+        }
+
+        public virtual void InitializePlant(IPlant plant)
+        {
+            plant.EnabledChanged += PlantOnEnabledChanged;
+        }
+
+        public virtual void InformInitializeStage()
+        {
+            
+        }
+
+        public virtual void InformDisplayStage()
+        {
+            
+        }
+
+        public virtual void InformClosingStage()
+        {
+            
+        }
+
+        
+
     }
 }
