00d57f68a28663286520f3c3fcee671baa2a8554
Alex Povar <user.home.0000@gmail.com>
2013-06-24 23:41:40 +0300

    Add processor to Startup pipeline

--- TrayGarden/Pipelines/Startup/PlantServicesInformInit.cs
+++ TrayGarden/Pipelines/Startup/PlantServicesInformInit.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using TrayGarden.Services.Engine;
+using TrayGarden.TypesHatcher;
 
 namespace TrayGarden.Pipelines.Startup
 {
@@ -9,7 +11,7 @@ namespace TrayGarden.Pipelines.Startup
     {
         public void Process(StartupArgs args)
         {
-            
+            HatcherGuide<IServicesSteward>.Instance.InformInitializeStage();
         }
     }
 }
