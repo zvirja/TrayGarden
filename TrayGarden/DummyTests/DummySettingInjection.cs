bcb2ccb11ec170660b63a613976b14ec535b2c25
Alex Povar <user.home.0000@gmail.com>
2013-11-12 00:59:30 +0200

    Dummy update

--- TrayGarden/DummyTests/DummySettingInjection.cs
+++ TrayGarden/DummyTests/DummySettingInjection.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using JetBrains.Annotations;
 using TrayGarden.Configuration.ApplicationConfiguration.GetApplicationConfigStepPipeline;
+using TrayGarden.Helpers;
+using TrayGarden.Resources;
 using TrayGarden.TypesHatcher;
 using TrayGarden.UI;
 using TrayGarden.UI.Common.Commands;
 using TrayGarden.UI.Configuration;
 using TrayGarden.UI.Configuration.Stuff;
+using TrayGarden.UI.Configuration.Stuff.ExtentedEntry;
 
 namespace TrayGarden.DummyTests
 {
@@ -27,16 +32,18 @@ namespace TrayGarden.DummyTests
 
     protected ConfigurationEntryVMBase GetActionConfigurationEntry()
     {
-      return new ConfigurationEntryForAction(new ActionAwarePlayer("Dummy setting", "Dummy action", delegate(object obj)
+      var realPlayer = new ActionAwarePlayer("Dummy setting", "Dummy action", delegate(object obj)
       {
         HatcherGuide<IUIManager>.Instance.OKMessageBox("Dummy action","Dummy action performed");
-      }));
+      });
+     
+      return new ConfigurationEntryForAction(realPlayer);
     }
 
     public class ActionAwarePlayer : ConfigurationAwarePlayer
     {
       public ActionAwarePlayer(string settingName, string actionName, Action<object> action)
-        : base(settingName, false, false)
+        : base(settingName, true, false)
       {
         base.ActionTitle = actionName;
         base.Action = new RelayCommand(action, true);
