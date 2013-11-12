b2cf58ef0ceb7754c33a31137f9c9c2976ae37f9
Yuriy Bogomolov <cronus.cpp@gmail.com>
2013-11-12 10:45:55 +0200

    Implemented SmallApplicationLauncher plant
    This plant allows one to launch any application from the specified
    folder.

---
+++ SmallApplicationLauncher/ContextMenuExtender.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using TrayGarden.Services.PlantServices.GlobalMenu.Core.ContextMenuCollecting;
+using System.Windows.Forms;
+
+namespace SmallApplicationLauncher
+{
+  public class ContextMenuExtender : TrayGarden.Reception.Services.IExtendsGlobalMenu
+  {
+    public bool FillProvidedContextMenuBuilder(IMenuEntriesAppender menuAppender)
+    {
+      foreach (KeyValuePair<string, string> application in UserConfiguration.Configuration.Applications)
+      {
+        KeyValuePair<string, string> app = application;
+        
+        // YBO: When the issue #7 will be fixed, feel free to use this line instead:
+        // menuAppender.AppentMenuStripItem(application.Key, null, (sender, args) => Process.Start(app.Value));
+
+        var appender = menuAppender as MenuEntriesAppender;
+        if (appender != null)
+        {
+          appender.OutputItems.Add(new ToolStripMenuItem(application.Key, null, (sender, args) => Process.Start(app.Value)));
+        }
+      }
+
+      return true;
+    }
+  }
+}
---
+++ SmallApplicationLauncher/Launcher.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TrayGarden.Reception;
+
+namespace SmallApplicationLauncher
+{
+  public class Launcher : IPlant, IServicesDelegation
+  {
+    public string HumanSupportingName { get; private set; }
+    public string Description { get; private set; }
+
+    public Launcher()
+    {
+      HumanSupportingName = "Small Application Launcher";
+      Description = @"This plant provides an ability to specify path to the folder with small applications and display a list of such applications in the context menu.";
+    }
+
+    public void Initialize()
+    {
+    }
+
+    public void PostServicesInitialize()
+    {
+    }
+
+    public List<object> GetServiceDelegates()
+    {
+      var result = new List<object>
+      {
+        UserConfiguration.Configuration,
+        new ContextMenuExtender()
+      };
+      return result;
+    }
+  }
+}
