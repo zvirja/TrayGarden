003bd247429f7df8f7aeab1cb43091aaeac272b0
Alex Povar <user.home.0000@gmail.com>
2014-01-23 23:43:04 +0200

    Implemented debugging tracing for SettingsStorage
    In order to enable debugging traces the EnableDebuggingTraces bool
    setting of the SettingsStorage should be set to true.

--- TrayGarden/RuntimeSettings/Provider/SettingsStorage.cs
+++ TrayGarden/RuntimeSettings/Provider/SettingsStorage.cs
@@ -1,107 +1,120 @@
-﻿using System;
+﻿#region
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Xml.Serialization;
+
 using JetBrains.Annotations;
+
 using TrayGarden.Configuration;
 using TrayGarden.Diagnostics;
 using TrayGarden.Helpers;
+
+#endregion
 
 namespace TrayGarden.RuntimeSettings.Provider
 {
   [UsedImplicitly]
   public class SettingsStorage : ISettingsStorage
   {
-    protected IContainer ResolvedRootContainer { get; set; }
+    #region Constructors and Destructors
+
+    public SettingsStorage()
+    {
+      this.FileName = "RuntimeSettings.xml";
+      this.UseLocalFolder = true;
+      this.EnableDebuggingTraces = false;
+    }
+
+    #endregion
+
+    #region Public Properties
+
+    public virtual bool EnableDebuggingTraces { get; set; }
 
     public virtual string FileName { get; set; }
 
     public virtual bool UseLocalFolder { get; set; }
+
+    #endregion
+
+    #region Properties
+
     protected IObjectFactory ContainerFactory { get; set; }
 
+    protected IContainer ResolvedRootContainer { get; set; }
 
-    public SettingsStorage()
+    #endregion
+
+    #region Public Methods and Operators
+
+    public virtual IContainer GetRootContainer()
     {
-      FileName = "RuntimeSettings.xml";
-      UseLocalFolder = true;
+      return this.ResolvedRootContainer;
     }
 
     [UsedImplicitly]
     public void Initialize(IObjectFactory containerFactory)
     {
       Assert.ArgumentNotNull(containerFactory, "containerFactory");
-      ContainerFactory = containerFactory;
-    }
-
-
-    public virtual IContainer GetRootContainer()
-    {
-      return ResolvedRootContainer;
+      this.ContainerFactory = containerFactory;
     }
 
     public virtual void LoadSettings()
     {
-      string storageFilePath = GetFilePath();
-      Bucket rootBucket = DeserializeStorageFile(storageFilePath) ?? new Bucket();
-      var rootContainer = BuildContainerFromBucket(rootBucket);
-      ResolvedRootContainer = rootContainer;
+      string storageFilePath = this.GetFilePath();
+      Bucket rootBucket = this.DeserializeStorageFile(storageFilePath) ?? new Bucket();
+      var rootContainer = this.BuildContainerFromBucket(rootBucket);
+      this.ResolvedRootContainer = rootContainer;
     }
-
 
     public virtual bool SaveSettings()
     {
-      Bucket rootBucket = BuildBucketFromContainer(ResolvedRootContainer);
+      Bucket rootBucket = this.BuildBucketFromContainer(this.ResolvedRootContainer);
       if (rootBucket == null)
+      {
         return false;
-      bool result = SerializeToStorageFile(rootBucket);
+      }
+      bool result = this.SerializeToStorageFile(rootBucket);
       return result;
     }
 
-    protected virtual string GetFilePath()
-    {
-      string folderName = null;
-      if (!UseLocalFolder)
-      {
-        folderName = Settings.ApplicationDataFolderName;
-        if (!folderName.IsNullOrEmpty())
-        {
-          folderName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
-                                    folderName);
-        }
-      }
-      if (folderName.IsNullOrEmpty())
-        folderName = DirectoryHelper.CurrentDirectory;
-      Assert.IsNotNullOrEmpty(folderName, "Folder name shouldn't be unresolved");
-      return Path.Combine(folderName, FileName);
-    }
+    #endregion
 
-    protected virtual IContainer BuildContainerFromBucket(Bucket rootBucket)
-    {
-      Dictionary<string, string> settings = rootBucket.Settings.ToDictionary(settingPair => settingPair.Key,
-                                                                             settingPair => settingPair.Value);
-      var subcontainers = rootBucket.InnerBuckets.Select(BuildContainerFromBucket).ToList();
-      var newContainer = ContainerFactory.GetPurelyNewObject() as IContainer;
-      Assert.IsNotNull(newContainer, "Wrong container factory");
-      newContainer.InitializeFromCollections(rootBucket.Name, settings, subcontainers);
-      return newContainer;
-    }
+    #region Methods
 
     protected virtual Bucket BuildBucketFromContainer(IContainer rootContainer)
     {
       if (rootContainer == null)
+      {
         return null;
+      }
       var bucket = new Bucket();
       bucket.Name = rootContainer.Name;
       bucket.Settings =
-          rootContainer.GetPresentStringSettingNames()
-                       .Select(x => new StringStringPair(x, rootContainer.GetStringSetting(x)))
-                       .ToList();
+        rootContainer.GetPresentStringSettingNames()
+          .Select(x => new StringStringPair(x, rootContainer.GetStringSetting(x)))
+          .ToList();
       bucket.InnerBuckets =
-          rootContainer.GetPresentSubContainerNames()
-                       .Select(x => BuildBucketFromContainer(rootContainer.GetNamedSubContainer(x)))
-                       .ToList();
+        rootContainer.GetPresentSubContainerNames()
+          .Select(x => this.BuildBucketFromContainer(rootContainer.GetNamedSubContainer(x)))
+          .ToList();
       return bucket;
+    }
+
+    protected virtual IContainer BuildContainerFromBucket(Bucket rootBucket)
+    {
+      Dictionary<string, string> settings = rootBucket.Settings.ToDictionary(
+        settingPair => settingPair.Key,
+        settingPair => settingPair.Value);
+      var subcontainers = rootBucket.InnerBuckets.Select(this.BuildContainerFromBucket).ToList();
+      var newContainer = this.ContainerFactory.GetPurelyNewObject() as IContainer;
+      Assert.IsNotNull(newContainer, "Wrong container factory");
+      newContainer.InitializeFromCollections(rootBucket.Name, settings, subcontainers);
+      return newContainer;
     }
 
     protected virtual Bucket DeserializeStorageFile(string fileName)
@@ -109,11 +122,17 @@ namespace TrayGarden.RuntimeSettings.Provider
       try
       {
         if (!File.Exists(fileName))
+        {
           return new Bucket();
+        }
         using (var streamReader = new StreamReader(fileName))
         {
-          XmlSerializer serializer = GetBucketXmlSerializer();
+          XmlSerializer serializer = this.GetBucketXmlSerializer();
           Bucket resultObject = serializer.Deserialize(streamReader) as Bucket;
+          if (this.EnableDebuggingTraces)
+          {
+            this.TraceDebugStreamContent(streamReader.BaseStream,"ReadOp");
+          }
           return resultObject;
         }
       }
@@ -125,19 +144,52 @@ namespace TrayGarden.RuntimeSettings.Provider
       }
     }
 
+    protected virtual XmlSerializer GetBucketXmlSerializer()
+    {
+      return new XmlSerializer(typeof(Bucket));
+    }
+
+    protected virtual string GetFilePath()
+    {
+      string folderName = null;
+      if (!this.UseLocalFolder)
+      {
+        folderName = Settings.ApplicationDataFolderName;
+        if (!folderName.IsNullOrEmpty())
+        {
+          folderName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), folderName);
+        }
+      }
+      if (folderName.IsNullOrEmpty())
+      {
+        folderName = DirectoryHelper.CurrentDirectory;
+      }
+      Assert.IsNotNullOrEmpty(folderName, "Folder name shouldn't be unresolved");
+      return Path.Combine(folderName, this.FileName);
+    }
 
     protected virtual bool SerializeToStorageFile(Bucket bucket)
     {
-      string filePath = GetFilePath();
+      string filePath = this.GetFilePath();
       try
       {
         var parentDirectory = Directory.GetParent(filePath);
         if (!parentDirectory.Exists)
+        {
           Directory.CreateDirectory(parentDirectory.FullName);
+        }
         using (var streamWriter = new StreamWriter(filePath, false))
         {
-          XmlSerializer serializer = GetBucketXmlSerializer();
+          XmlSerializer serializer = this.GetBucketXmlSerializer();
           serializer.Serialize(streamWriter, bucket);
+          if (this.EnableDebuggingTraces)
+          {
+            using (var memStream = new MemoryStream())
+            {
+              serializer.Serialize(memStream, bucket);
+              this.TraceDebugStreamContent(memStream, "SaveOp");
+            }
+          }
         }
         return true;
       }
@@ -148,9 +200,34 @@ namespace TrayGarden.RuntimeSettings.Provider
       }
     }
 
-    protected virtual XmlSerializer GetBucketXmlSerializer()
+    protected virtual void TraceDebugStreamContent(Stream streamToTrace, string fileNameSuffix)
     {
-      return new XmlSerializer(typeof(Bucket));
+      try
+      {
+        string debugTracesDirectory = Path.Combine(DirectoryHelper.CurrentDirectory, "SettingsStorageDebugTraces");
+        Directory.CreateDirectory(debugTracesDirectory);
+        string traceFileName = "{0} --- {1}.txt".FormatWith(
+          DateTime.Now.ToString("yyyy-MM-dd HH-mm-ss-ffff"),
+          fileNameSuffix);
+        using (FileStream traceFileStream = File.Create(Path.Combine(debugTracesDirectory, traceFileName)))
+        {
+          streamToTrace.Seek(0, SeekOrigin.Begin);
+          streamToTrace.CopyTo(traceFileStream);
+
+          var streamWriter = new StreamWriter(traceFileStream);
+          streamWriter.WriteLine();
+          streamWriter.WriteLine();
+          streamWriter.WriteLine();
+          streamWriter.WriteLine("Current stack trace:");
+          streamWriter.WriteLine(Environment.StackTrace);
+        }
+      }
+      catch (Exception ex)
+      {
+        Log.Error("Enable to perform trace logging for SettingsStorage", ex, this);
+      }
     }
+
+    #endregion
   }
 }
