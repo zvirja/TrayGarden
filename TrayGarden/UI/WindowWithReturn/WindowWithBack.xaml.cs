a7f4a2ae7dd6cb7f6ce934ed1f13c89467329fc4
Alex Povar <user.home.0000@gmail.com>
2014-02-02 21:04:10 +0200

    MainWindow BringToFront Logic change

--- TrayGarden/UI/WindowWithReturn/WindowWithBack.xaml.cs
+++ TrayGarden/UI/WindowWithReturn/WindowWithBack.xaml.cs
@@ -1,18 +1,23 @@
-﻿using System;
+﻿#region
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows;
-using System.Windows.Media;
+
 using JetBrains.Annotations;
+
 using TrayGarden.Diagnostics;
+using TrayGarden.Helpers;
 using TrayGarden.Resources;
 using TrayGarden.RuntimeSettings;
 using TrayGarden.TypesHatcher;
 using TrayGarden.UI.Common.VMtoVMapping;
-using TrayGarden.Helpers;
+
+#endregion
 
 namespace TrayGarden.UI.WindowWithReturn
 {
@@ -21,13 +26,48 @@ namespace TrayGarden.UI.WindowWithReturn
   /// </summary>
   public partial class WindowWithBack : Window, IVMtoVMappingsSource, IWindowWithBack
   {
-    protected static ISettingsBox WindowWithBackSettingsBox { get; set; }
+    #region Static Fields
+
     protected static bool _exitOnClose;
 
+    #endregion
+
+    /*protected MappingsBasedContentValueConverter ViewModelToViewConverter { get; set; }*/
+
+    #region Fields
+
+    protected string iconResourceKey;
+
+    #endregion
+
+    #region Constructors and Destructors
+
+    static WindowWithBack()
+    {
+      WindowWithBackSettingsBox =
+        HatcherGuide<IRuntimeSettingsManager>.Instance.SystemSettings.GetSubBox("windowWithBackSettingsBox");
+      _exitOnClose = WindowWithBackSettingsBox.GetBool("exitOnClose", false);
+    }
+
+    public WindowWithBack()
+    {
+      this.InitializeComponent();
+      this.IconResourceKey = "gardenIconV5";
+      this.StateToRestore = this.WindowState;
+      this.Hide();
+      this.SetIcon();
+    }
+
+    #endregion
+
+    #region Public Properties
 
     public static bool ExitOnClose
     {
-      get { return _exitOnClose; }
+      get
+      {
+        return _exitOnClose;
+      }
       set
       {
         _exitOnClose = value;
@@ -35,76 +75,95 @@ namespace TrayGarden.UI.WindowWithReturn
       }
     }
 
-    static WindowWithBack()
-    {
-      WindowWithBackSettingsBox =
-          HatcherGuide<IRuntimeSettingsManager>.Instance.SystemSettings.GetSubBox("windowWithBackSettingsBox");
-      _exitOnClose = WindowWithBackSettingsBox.GetBool("exitOnClose", false);
-    }
-
-    /*protected MappingsBasedContentValueConverter ViewModelToViewConverter { get; set; }*/
-    protected string iconResourceKey;
-
-    protected List<IViewModelToViewMapping> Mappings { get; set; }
-    protected WindowState StateToRestore { get; set; }
-
     public string IconResourceKey
     {
-      get { return iconResourceKey; }
+      get
+      {
+        return this.iconResourceKey;
+      }
       set
       {
-        iconResourceKey = value;
-        SetIcon();
+        this.iconResourceKey = value;
+        this.SetIcon();
       }
     }
 
-    public WindowWithBack()
+    public bool IsCurrentlyDisplayed
     {
-      InitializeComponent();
-      IconResourceKey = "gardenIconV5";
-      StateToRestore = this.WindowState;
-      Hide();
-      SetIcon();
+      get
+      {
+        return this.DataContext != null;
+      }
     }
 
-    public bool IsCurrentlyDisplayed { get { return DataContext != null; } }
+    #endregion
+
+    #region Properties
+
+    protected static ISettingsBox WindowWithBackSettingsBox { get; set; }
+
+    protected List<IViewModelToViewMapping> Mappings { get; set; }
+
+    protected WindowState StateToRestore { get; set; }
+
+    #endregion
+
+    #region Public Methods and Operators
+
+    public void BringToFront()
+    {
+      if (this.IsCurrentlyDisplayed)
+      {
+        if (this.WindowState == WindowState.Minimized)
+        {
+          this.WindowState = WindowState.Normal;
+        }
+        this.Activate();
+      }
+    }
+
+    public virtual List<IViewModelToViewMapping> GetMappings()
+    {
+      return this.Mappings ?? new List<IViewModelToViewMapping>();
+    }
 
     public virtual void Initialize([NotNull] List<IViewModelToViewMapping> mvtovmappings)
     {
       Assert.ArgumentNotNull(mvtovmappings, "mvtovmappings");
-      Mappings = mvtovmappings;
+      this.Mappings = mvtovmappings;
     }
 
     public virtual void PrepareAndShow(WindowWithBackVM viewModel)
     {
-      CleanupAndDisposeDataContext();
+      this.CleanupAndDisposeDataContext();
       this.DataContext = viewModel;
-      viewModel.SizePozitionProvider = SizePozitionProvider;
+      viewModel.SizePozitionProvider = this.SizePozitionProvider;
       viewModel.PrepareToShow();
-      SetSizeAndPos(viewModel);
-      Show();
-      WindowState = StateToRestore;
+      this.SetSizeAndPos(viewModel);
+      this.Show();
+      this.WindowState = this.StateToRestore;
     }
 
-    public void BringToFront()
+    #endregion
+
+    #region Methods
+
+    protected virtual void CleanupAndDisposeDataContext()
     {
-      if (IsCurrentlyDisplayed)
-        this.Activate();
+      var currentDataContextAsDisposable = this.DataContext as IDisposable;
+      this.DataContext = null;
+      if (currentDataContextAsDisposable != null)
+      {
+        currentDataContextAsDisposable.Dispose();
+      }
     }
-
-
-    public virtual List<IViewModelToViewMapping> GetMappings()
-    {
-      return Mappings ?? new List<IViewModelToViewMapping>();
-    }
-
 
     protected override void OnClosing(CancelEventArgs e)
     {
       e.Cancel = !ExitOnClose;
-      Hide();
+      this.Hide();
 
-      CleanupAndDisposeDataContext();
+      this.CleanupAndDisposeDataContext();
       base.OnClosing(e);
     }
 
@@ -112,63 +171,68 @@ namespace TrayGarden.UI.WindowWithReturn
     {
       if (this.WindowState == WindowState.Minimized && ExitOnClose)
       {
-        Close();
+        this.Close();
       }
       else
       {
-        StateToRestore = WindowState;
+        this.StateToRestore = this.WindowState;
       }
       base.OnStateChanged(e);
     }
 
-    protected virtual void CleanupAndDisposeDataContext()
+    protected void SetIcon()
     {
-      var currentDataContextAsDisposable = DataContext as IDisposable;
-      DataContext = null;
-      if (currentDataContextAsDisposable != null)
-        currentDataContextAsDisposable.Dispose();
+      if (this.IconResourceKey.IsNullOrEmpty())
+      {
+        return;
+      }
+      Icon resource = HatcherGuide<IResourcesManager>.Instance.GetIconResource(this.IconResourceKey, null);
+      if (resource == null)
+      {
+        return;
+      }
+      this.Icon = ImageHelper.Bitmap2BitmapImage(resource.ToBitmap());
     }
 
     protected virtual void SetSizeAndPos(WindowWithBackVM viewModel)
     {
       if (!viewModel.SizePropertiesAreValid)
+      {
         return;
+      }
       this.Top = viewModel.Top;
       this.Left = viewModel.Left;
       this.Height = viewModel.Height;
       this.Width = viewModel.Width;
-      WindowState = StateToRestore = viewModel.Maximized ? WindowState.Maximized : WindowState.Normal;
+      this.WindowState = this.StateToRestore = viewModel.Maximized ? WindowState.Maximized : WindowState.Normal;
     }
 
-    protected virtual bool SizePozitionProvider(out double top, out double left, out double width, out double height, out bool maximized)
+    protected virtual bool SizePozitionProvider(
+      out double top,
+      out double left,
+      out double width,
+      out double height,
+      out bool maximized)
     {
-      if (WindowState == WindowState.Maximized)
+      if (this.WindowState == WindowState.Maximized)
       {
-        top = RestoreBounds.Top;
-        left = RestoreBounds.Left;
-        height = RestoreBounds.Height;
-        width = RestoreBounds.Width;
+        top = this.RestoreBounds.Top;
+        left = this.RestoreBounds.Left;
+        height = this.RestoreBounds.Height;
+        width = this.RestoreBounds.Width;
         maximized = true;
       }
       else
       {
-        top = Top;
-        left = Left;
-        height = Height;
-        width = Width;
+        top = this.Top;
+        left = this.Left;
+        height = this.Height;
+        width = this.Width;
         maximized = false;
       }
       return true;
     }
 
-    protected void SetIcon()
-    {
-      if (IconResourceKey.IsNullOrEmpty())
-        return;
-      Icon resource = HatcherGuide<IResourcesManager>.Instance.GetIconResource(IconResourceKey, null);
-      if (resource == null)
-        return;
-      this.Icon = ImageHelper.Bitmap2BitmapImage(resource.ToBitmap());
-    }
+    #endregion
   }
 }
