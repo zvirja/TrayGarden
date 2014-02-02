7a1c0169231a94d6e629bc69fb7375a1090d6c19
Alex Povar <user.home.0000@gmail.com>
2014-02-02 04:18:16 +0200

    LifecycleObserver reformat.

--- TrayGarden/LifeCycle/LifecycleObserver.cs
+++ TrayGarden/LifeCycle/LifecycleObserver.cs
@@ -1,80 +1,59 @@
-﻿using System;
+﻿#region
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Windows;
+
 using TrayGarden.Diagnostics;
 using TrayGarden.Pipelines.RestartApp;
 using TrayGarden.Pipelines.Shutdown;
 using TrayGarden.Pipelines.Startup;
 using TrayGarden.Resources;
 
+#endregion
+
 namespace TrayGarden.LifeCycle
 {
   public class LifecycleObserver
   {
+    #region Properties
 
     protected static LifecycleObserver Observer { get; set; }
+
+    #endregion
+
+    #region Public Methods and Operators
 
     public static void NotifyStartup(string[] args)
     {
       if (Observer != null)
+      {
         return;
+      }
       Observer = new LifecycleObserver();
       Observer.LoadLog4NetFromResources();
       Observer.SetAssembliesHook();
       Observer.NotifyStartupInternal(args);
     }
 
-
-
     public static void RestartApp(string[] paramsToAdd)
     {
       RestartAppPipeline.Run(paramsToAdd);
     }
 
+    #endregion
 
-    protected virtual void NotifyStartupInternal(string[] args)
-    {
-      try
-      {
-        Application.Current.DispatcherUnhandledException += Current_DispatcherUnhandledException;
-        StartupPipeline.Run(args);
-        Application.Current.Exit += ApplicationExit;
-      }
-      catch (Exception ex)
-      {
-        Log.Error("Error at startup", ex, this);
-        Application.Current.Shutdown(1);
-      }
-    }
-
-    void Current_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
-    {
-      Log.Error("Thrown exception wasn't catched. Application will be closed", e.Exception, typeof(Application));
-      e.Handled = true;
-      Application.Current.Shutdown(1);
-    }
+    #region Methods
 
     protected virtual void ApplicationExit(object sender, ExitEventArgs e)
     {
       if (e.ApplicationExitCode == 0)
+      {
         ShutdownPipeline.Run();
-    }
-
-    protected virtual void LoadLog4NetFromResources()
-    {
-      var alreadyLoaded = AppDomain.CurrentDomain.GetAssemblies().FirstOrDefault(x => x.FullName.StartsWith("log4net"));
-      if (alreadyLoaded != null)
-        return;
-      byte[] log4Net = GlobalResources.log4net;
-      Assembly.Load(log4Net);
-    }
-
-    protected virtual void SetAssembliesHook()
-    {
-      AppDomain.CurrentDomain.AssemblyResolve += CurrentDomainOnAssemblyResolve;
+      }
     }
 
     protected Assembly CurrentDomainOnAssemblyResolve(object sender, ResolveEventArgs args)
@@ -84,6 +63,46 @@ namespace TrayGarden.LifeCycle
           .FirstOrDefault(x => x.FullName.StartsWith(args.Name, StringComparison.OrdinalIgnoreCase));
     }
 
+    protected virtual void LoadLog4NetFromResources()
+    {
+      var alreadyLoaded = AppDomain.CurrentDomain.GetAssemblies().FirstOrDefault(x => x.FullName.StartsWith("log4net"));
+      if (alreadyLoaded != null)
+      {
+        return;
+      }
+      byte[] log4Net = GlobalResources.log4net;
+      Assembly.Load(log4Net);
+    }
 
+    protected virtual void NotifyStartupInternal(string[] args)
+    {
+      try
+      {
+        Application.Current.DispatcherUnhandledException += this.Current_DispatcherUnhandledException;
+        StartupPipeline.Run(args);
+        Application.Current.Exit += this.ApplicationExit;
+      }
+      catch (Exception ex)
+      {
+        Log.Error("Error at startup", ex, this);
+        Application.Current.Shutdown(1);
+      }
+    }
+
+    protected virtual void SetAssembliesHook()
+    {
+      AppDomain.CurrentDomain.AssemblyResolve += this.CurrentDomainOnAssemblyResolve;
+    }
+
+    private void Current_DispatcherUnhandledException(
+      object sender,
+      System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+    {
+      Log.Error("Thrown exception wasn't catched. Application will be closed", e.Exception, typeof(Application));
+      e.Handled = true;
+      Application.Current.Shutdown(1);
+    }
+
+    #endregion
   }
 }
