40829d6d5e3f8467630e8f321eda187c798cb08a
Alex Povar <user.home.0000@gmail.com>
2014-02-02 20:38:30 +0200

    Added SingleInstanceMonitor release before app restart.

---
+++ TrayGarden/Pipelines/RestartApp/100StopSingleInstanceMonitor.cs
@@ -0,0 +1,31 @@
+﻿#region
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+using JetBrains.Annotations;
+
+using TrayGarden.Helpers;
+using TrayGarden.TypesHatcher;
+
+#endregion
+
+namespace TrayGarden.Pipelines.RestartApp
+{
+  public class StopSingleInstanceMonitor
+  {
+    #region Public Methods and Operators
+
+    [UsedImplicitly]
+    public virtual void Process(RestartAppArgs args)
+    {
+      ManualResetEventSlim monitor = HatcherGuide<ISingleInstanceMonitor>.Instance.EnqueueMonitorDisabling();
+      monitor.Wait();
+    }
+
+    #endregion
+  }
+}
---
+++ TrayGarden/Pipelines/RestartApp/200SimpleAppRestart.cs
@@ -0,0 +1,28 @@
+﻿#region
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+using JetBrains.Annotations;
+
+#endregion
+
+namespace TrayGarden.Pipelines.RestartApp
+{
+  public class SimpleAppRestart
+  {
+    #region Public Methods and Operators
+
+    [UsedImplicitly]
+    public virtual void Process(RestartAppArgs args)
+    {
+      System.Diagnostics.Process.Start(Application.ResourceAssembly.Location, string.Join(" ", args.ParamsToAdd));
+      Application.Current.Shutdown();
+    }
+
+    #endregion
+  }
+}
